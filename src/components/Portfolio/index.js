c5de0f3c42e425f12f9ebbfe5db8af74fcec9922
Quinn-H <quinn.hu618@gmail.com>
2017-05-06 02:42:34 +1200

    fix bug

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,33 +1,9 @@
 import React, { Component } from 'react'
-import P from 'react-particles-js'
 
 export default class Portfolio extends Component {
   render () {
     return (
-      <div>
-        <P
-          params={{
-            particles: {
-              number: {
-                value: 30
-              },
-              line_linked: {
-                shadow: {
-                  enable: false,
-                  color: '#3CA9D1',
-                  blur: 5
-                }
-              }
-            }
-          }}
-          style={{
-            position: 'fixed',
-            top: -500,
-            left: 0,
-            width: '100%',
-            height: '100%'
-          }} />
-      </div>
+      <div />
     )
   }
 }
